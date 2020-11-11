2554333f081029b80d283fac400e41b9d8c9a944
ivncastillo <ivncastillo@gmail.com>
2020-11-10 22:04:08 -0300

    parte2

--- desafio_operaciones/operaciones_bbdd.sql
+++ desafio_operaciones/operaciones_bbdd.sql
@@ -1,3 +1,7 @@
+---------------------
+-- PARTE 1 ---------
+---------------------
+
 -- 1. Crear Base de datos posts (1 punto)
 CREATE DATABASE post;
 
@@ -8,8 +12,8 @@ id INT
 ,fecha_creacion DATE
 ,contenido VARCHAR(255)
 ,descripcion VARCHAR(255)
-)
-;
+, PRIMARY KEY (id)
+);
 
 -- 3. Insertar 3 post, 2 para el usuario "Pamela" y uno para "Carlos" (0.6 puntos)
 INSERT INTO post
@@ -55,9 +59,65 @@ VALUES ('5', 'Pedro', '2020-11-10' , 'Este es un contenido', 'esta es una descri
 
 -- 7. Eliminar el post de Carlos (1 punto)
 DELETE FROM post
-WHERE usuario='Carlos'
-;
+WHERE nombre_usuario='Carlos';
+
 -- 8. Ingresar un nuevo post para el usuario "Carlos" (0.6 puntos)
 INSERT INTO post
 (id, nombre_usuario, fecha_creacion, contenido, descripcion, titulo)
 VALUES ('6', 'Carlos', '2020-11-10' , 'Este es un contenido', 'esta es una descriocion', 'Este es un titulo');
+
+---------------------
+-- PARTE 2 ---------
+---------------------
+
+-- 9. Crear una nueva tabla, llamada comentarios, con los atributos id, fecha y hora de creación, contenido, 
+-- que se relacione con la tabla posts. (1 punto)
+CREATE TABLE llamada_comentarios(
+id INT
+,fecha DATE
+,hora_creacion VARCHAR(25)
+,contenido VARCHAR(255)
+);
+
+-- 10. Crear 2 comentarios para el post de "Pamela" y 4 para "Carlos" (0.4 puntos)
+INSERT INTO llamada_comentarios
+(id, fecha, hora_creacion, contenido)
+VALUES (1, '2020-11-10', '21:54', 'este es el contenido');
+
+INSERT INTO llamada_comentarios
+(id,  fecha, hora_creacion, contenido)
+VALUES (1, '2020-11-10', '21:55', 'este es el contenido');
+
+INSERT INTO llamada_comentarios
+(id,  fecha, hora_creacion, contenido)
+VALUES (2, '2020-11-10', '21:56', 'este es el contenido');
+
+INSERT INTO llamada_comentarios
+(id, fecha, hora_creacion, contenido)
+VALUES (2, '2020-11-10', '21:57', 'este es el contenido');
+
+-- 11. Crear un nuevo post para "Margarita" (1 punto)
+INSERT INTO post
+(id, nombre_usuario, fecha_creacion, contenido, descripcion, titulo)
+VALUES ('7', 'Margarita', '2020-11-10' , 'Este es un contenido', 'esta es una descriocion', 'Este es un titulo');
+
+-- 12. Ingresar 5 comentarios para el post de Margarita. (1 punto)
+INSERT INTO llamada_comentarios
+(id,  fecha, hora_creacion, contenido)
+VALUES (7, '2020-11-10', '21:57', 'este es el contenido');
+
+INSERT INTO llamada_comentarios
+(id, fecha, hora_creacion, contenido)
+VALUES (7, '2020-11-10', '21:58', 'este es el contenido');
+
+INSERT INTO llamada_comentarios
+(id, fecha, hora_creacion, contenido)
+VALUES (7, '2020-11-10', '21:59', 'este es el contenido');
+
+INSERT INTO llamada_comentarios
+(id, fecha, hora_creacion, contenido)
+VALUES (7, '2020-11-10', '22:00', 'este es el contenido');
+
+INSERT INTO llamada_comentarios
+(id, fecha, hora_creacion, contenido)
+VALUES (7, '2020-11-10', '22:01', 'este es el contenido');
